815bdf76110d57bdd6f15842356f3ff3cd0a5258
Harrison Ng'ang'a <harrykigs2003@gmai.com>
2025-04-07 19:58:06 +0300

    Implements the index.php to detect the landing page

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
- 
+<?php
+// index.php
+require_once 'config/init.php';
+
+if (isLoggedIn()) {
+    header("Location: views/dashboard.php");
+} else {
+    header("Location: views/auth/login.php");
+}
+exit; 
